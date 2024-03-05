39a259dfd773ed7d0bad32546f12164f722d2a8f
Abdul Ahad shaikh <mickyahad8955@gmail.com>
2024-03-06 02:11:07 +0500

    stripping name

---
+++ stripping_names.js
@@ -0,0 +1,5 @@
+"use strict";
+let whitespace_name = "\n\t AHAD \t\n";
+console.log(whitespace_name);
+let withoutWhitespace_name = whitespace_name.trim();
+console.log(withoutWhitespace_name);
---
+++ stripping_names.ts
@@ -0,0 +1,4 @@
+let whitespace_name= "\n\t AHAD \t\n";
+console.log(whitespace_name);
+let withoutWhitespace_name=whitespace_name.trim();
+console.log(withoutWhitespace_name);
