429370f163c799e0f13f492447ddbe2231f196e3
Abdul Ahad shaikh <mickyahad8955@gmail.com>
2024-03-05 16:00:59 +0500

    Question 2

---
+++ question2.ts
@@ -0,0 +1,8 @@
+let myName:string;
+myName="A.Ahad";
+let uppercaseName=myName.toUpperCase();
+console.log("Uppercase =",uppercaseName);
+let lowercaseName=myName.toLowerCase();
+console.log("Lowercase =",lowercaseName);
+let titlecasemyName=myName.replace(/(^|\s)\S/g,(char)=>char.toUpperCase());
+console.log("Titlecase =",titlecasemyName);
