70bcfb13a2bfaf35f6d2cf82fcae7af95ffaa175
Abdul Ahad shaikh <mickyahad8955@gmail.com>
2024-03-06 20:30:29 +0500

    array of friends

---
+++ names.js
@@ -0,0 +1,4 @@
+"use strict";
+let friendsnames;
+friendsnames = ['ABDULLAH', 'RAAHIM', 'HAZIQ', 'IBRAHEEM'];
+console.log(friendsnames[0], "\n", friendsnames[1], "\n", friendsnames[2], "\n", friendsnames[3]);
---
+++ names.ts
@@ -0,0 +1,3 @@
+let friendsnames:string[];
+friendsnames=['ABDULLAH','RAAHIM','HAZIQ','IBRAHEEM'];
+console.log(friendsnames[0],"\n",friendsnames[1],"\n",friendsnames[2],"\n",friendsnames[3]);
