0c7d48580c68f5a0bd54278bf510ec13ace31da3
Abdul Ahad shaikh <mickyahad8955@gmail.com>
2024-03-09 16:06:50 +0500

    greetings

---
+++ greetings.js
@@ -0,0 +1,7 @@
+"use strict";
+let guestList;
+guestList = ['"Ahmed"', '"Ahad"', '"Ali"', '"Ibraheem"'];
+console.log("Asslamo Alaikum!", guestList[0], ",I hope you are fine!");
+console.log("Asslamo Alaikum!", guestList[1], ",I hope you are fine!");
+console.log("Asslamo Alaikum!", guestList[2], ",I hope you are fine!");
+console.log("Asslamo Alaikum!", guestList[3], ",I hope you are fine!");
