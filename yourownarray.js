3b45ea1e03b437b9bfe72b664c87dcf24131e2b7
Abdul Ahad shaikh <mickyahad8955@gmail.com>
2024-03-09 19:42:49 +0500

    arrays

---
+++ yourownarray.js
@@ -0,0 +1,6 @@
+var fleet;
+fleet = ['Honda Civic', 'Land cruisers "V8"', 'Audi Etron GT', 'Toyota Hillux'];
+console.log("My Big Brother has own a", fleet[0]);
+console.log("My favorite car is ", fleet[2]);
+console.log(fleet[1], "is good for mountain running");
+console.log(fleet[3], "has a very powerful engine");
